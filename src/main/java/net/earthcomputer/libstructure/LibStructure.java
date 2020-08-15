61b20e214ed2e6bcaa4b268a97ffd5e54995a05a
Joe <burtonjae@hotmail.co.uk>
2020-08-15 14:28:12 +0100

    Update to 1.16.2-pre1 and add biome fix

--- src/main/java/net/earthcomputer/libstructure/LibStructure.java
+++ src/main/java/net/earthcomputer/libstructure/LibStructure.java
@@ -3,16 +3,24 @@ package net.earthcomputer.libstructure;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import net.earthcomputer.libstructure.impl.LibStructureImpl;
+import net.earthcomputer.libstructure.mixin.BiomeAccessor;
 import net.earthcomputer.libstructure.mixin.FlatChunkGeneratorConfigAccessor;
 import net.earthcomputer.libstructure.mixin.StructureFeatureAccessor;
 import net.earthcomputer.libstructure.mixin.StructuresConfigAccessor;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.chunk.StructureConfig;
 import net.minecraft.world.gen.chunk.StructuresConfig;
 import net.minecraft.world.gen.feature.ConfiguredStructureFeature;
 import net.minecraft.world.gen.feature.FeatureConfig;
 import net.minecraft.world.gen.feature.StructureFeature;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class LibStructure {
 
@@ -43,6 +51,22 @@ public class LibStructure {
         }
 
         LibStructureImpl.upToDateConfigs.clear();
+
+        for (Biome biome : BuiltinRegistries.BIOME) {
+            Map<Integer, List<StructureFeature<?>>> structureLists = ((BiomeAccessor) (Object) biome).getStructureLists();
+            if (!(structureLists instanceof HashMap)) {
+                // not guaranteed by the standard to be a mutable map
+                ((BiomeAccessor) (Object) biome).setStructureLists(structureLists = new HashMap<>(structureLists));
+            }
+            // not guaranteed by the standard to be mutable lists
+            structureLists.compute(step.ordinal(), (k, v) -> makeMutable(v)).add(structure);
+        }
+    }
+
+    private static List<StructureFeature<?>> makeMutable(List<StructureFeature<?>> mapValue) {
+        if (mapValue == null) return new ArrayList<>();
+        if (!(mapValue instanceof ArrayList)) return new ArrayList<>(mapValue);
+        return mapValue;
     }
 
     public static <FC extends FeatureConfig, F extends StructureFeature<FC>> void registerSurfaceAdjustingStructure(
