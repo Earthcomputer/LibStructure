90ea096a40f7697c78d14780a218801483a6e50f
Joe <burtonjae@hotmail.co.uk>
2020-08-15 14:36:37 +0100

    Update to 1.16.2-pre1 mappings

--- src/main/java/net/earthcomputer/libstructure/mixin/ChunkGeneratorSettingsMixin.java
+++ src/main/java/net/earthcomputer/libstructure/mixin/ChunkGeneratorSettingsMixin.java
@@ -1,16 +1,16 @@
 package net.earthcomputer.libstructure.mixin;
 
 import net.earthcomputer.libstructure.impl.IStructuresConfig;
-import net.minecraft.world.gen.chunk.ChunkGeneratorType;
+import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ChunkGeneratorType.class)
-public class ChunkGeneratorTypeMixin {
+@Mixin(ChunkGeneratorSettings.class)
+public class ChunkGeneratorSettingsMixin {
     @Inject(method = "method_30641", at = @At("RETURN"))
-    private static void onCreateCavesType(CallbackInfoReturnable<ChunkGeneratorType> ci) {
-        ((IStructuresConfig) ci.getReturnValue().getConfig()).libstructure_setHasDefaultFeatures();
+    private static void onCreateCavesType(CallbackInfoReturnable<ChunkGeneratorSettings> ci) {
+        ((IStructuresConfig) ci.getReturnValue().getStructuresConfig()).libstructure_setHasDefaultFeatures();
     }
 }
